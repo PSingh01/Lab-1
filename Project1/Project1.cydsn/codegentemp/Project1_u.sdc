b966ee88af0f3b63899b0aeb69d20bf9b0b27d33
Pavandeep Singh <Pavandeep Singh>
2016-09-27 15:18:56 -0400

    This is the entire Lab folder containing the project

---
+++ Project1/Project1.cydsn/codegentemp/Project1_u.sdc
@@ -0,0 +1,3 @@
+# Component constraints for C:\Users\Pavandeep\Documents\PSoC Creator\Project1\Project1.cydsn\TopDesign\TopDesign.cysch
+# Project: C:\Users\Pavandeep\Documents\PSoC Creator\Project1\Project1.cydsn\Project1.cyprj
+# Date: Tue, 20 Sep 2016 14:34:47 GMT
---
+++ Project1/Project1.cydsn/main.c
@@ -0,0 +1,28 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include <project.h>
+
+int main()
+{
+   
+
+    PWM_1_Start();
+    
+    
+    for(;;)
+    {
+       
+    }
+    
+}
+        
+    /* [] END OF FILE */
